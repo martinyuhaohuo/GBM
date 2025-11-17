972167f97def5eac52df449c166b06cf2c02b938
Yuhao Huo <martinyuhaohuo@gmail.com>
2025-11-17 15:14:41 +0000

    add version

--- pygbm/version.py
+++ pygbm/version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.post9'
-__version_tuple__ = version_tuple = (0, 0, 'post9')
+__version__ = version = '1.0.0.post0'
+__version_tuple__ = version_tuple = (1, 0, 0, 'post0')
 
-__commit_id__ = commit_id = 'g6dd67c582'
+__commit_id__ = commit_id = 'gc3ca216b0'
