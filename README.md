cfa95f2fcaead9cacfde8945537be60159794b26
Yuhao Huo <martinyuhaohuo@gmail.com>
2025-11-18 18:26:46 +0000

    modify readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GBM
 A Python package for Geometric Brownian motion simulations. 
+
+Installiation: pip install -e .
--- pygbm/version.py
+++ pygbm/version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '1.0.0.post3'
-__version_tuple__ = version_tuple = (1, 0, 0, 'post3')
+__version__ = version = '1.0.0.post8'
+__version_tuple__ = version_tuple = (1, 0, 0, 'post8')
 
-__commit_id__ = commit_id = 'gccb28be72'
+__commit_id__ = commit_id = 'g8c1bcec86'
